f441b0e3c2fafafc6305ff07ddfddb18107ba0b1
OasiBoi <ostally001@gmail.com>
2022-12-21 12:56:19 -0700

    separated code from document

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the data from the sheet
+window.onload = _ => {
+	// get the url
+	
+}
+document.getElementById("contact-button").onclick = _ => {
+	document.getElementById("contact").scrollIntoView({block:"end", behavior:"smooth"});
+}
