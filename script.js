60bda53fe35db8d9ecc6990096a4c8f7f6581a3d
OasiBoi <ostally001@gmail.com>
2023-01-06 11:39:32 -0700

    fixed item design in under catalog and added 2 items

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get the data from the sheet
 let prev_width = window.innerWidth;
-let response_threshold = 800;
+let response_threshold = 900;
 window.onload = _ => {
 	 // select ui based on screen size
 	let url = document.location.pathname.slice(1);
