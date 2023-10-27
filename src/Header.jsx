fe20bb423fd1b62f694894485389c5d5de4c91f4
itsOasi <ostally001@gmail.com>
2023-10-27 10:56:50 -0700

    UI tweaks

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,7 +10,9 @@ function Header() {
 		<div id='header'>
 			<img className='logo' src="./innova-title-11@2x.png"></img>
 			<div className='nav row'>
-				<Button href='https://instagram.com/roseling_gd?igshid=OGQ5ZDc2ODk2ZA==' msg={<Icons.Instagram className='icon' />}></Button>
+				<a href='https://instagram.com/roseling_gd?igshid=OGQ5ZDc2ODk2ZA=='>
+				<Icons.Instagram className='icon' href='https://instagram.com/roseling_gd?igshid=OGQ5ZDc2ODk2ZA==' />
+				</a>
 				<Button action="true" href="https://donate.stripe.com/cN24gP5PM1Z371mdQQ" msg="Support Us"></Button>
 			</div>
 		</div>
